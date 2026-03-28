63b6786141a0a322a502f3714e66760f50466ea9
treeform <treeform@users.noreply.github.com>
2026-03-28 04:20:25 +0000

    deploy: f7942a8c04119c0a082d5d6e972c34b78b18c16e

--- tools/gen_atlas.nim
+++
@@ -1,7 +0,0 @@
-import silky
-
-let builder = newAtlasBuilder(1024, 4)
-builder.addDir("tools/theme/", "tools/theme/")
-builder.addFont("tools/theme/IBMPlexSans.ttf", "H1", 28.0)
-builder.addFont("tools/theme/IBMPlexSans.ttf", "Default", 18.0)
-builder.write("tools/dist/atlas.png")
